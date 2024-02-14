3ce6881fd92ae3520023cbeb15695b114ee9436a
unknown <nukoolmu@ais.co.th>
2024-02-14 03:40:19 -0800

    update

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace webapp_lab14.Pages;
+namespace webapp_sam_lab13.Pages;
 
 public class PrivacyModel : PageModel
 {
